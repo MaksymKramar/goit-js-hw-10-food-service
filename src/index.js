42fac10042770b2d1e32846ecedd26a578dd9f8d
Maksym Kramar <maxim.kramar77@gmail.com>
2021-05-02 23:13:36 +0200

    Добавляет JS к проекту

--- src/index.js
+++ src/index.js
@@ -1,1 +1,76 @@
-import './sass/main.scss';
+import menu from './menu.json';
+
+import dish from './templates/dishes.hbs';
+
+const Theme = {
+  LIGHT: 'light-theme',
+  DARK: 'dark-theme',
+};
+
+// document.body.className = Theme.LIGHT;
+
+const switchToggle = document.querySelector('#theme-switch-toggle')
+const dishesMenu = document.querySelector('.js-menu')
+const markUp = dish(menu)
+
+
+dishesMenu.insertAdjacentHTML('beforeend', markUp)
+
+
+class BodyTheme{
+    constructor({ selector}) {
+        this.bodyRef = document.querySelector(selector)
+        this.bodyRef.className = Theme.LIGHT
+        this._init()   
+        
+    }
+     
+    _init() {
+        const persistedThemeState = localStorage.getItem('Theme-DARK')
+                
+        if (persistedThemeState !== null) {
+            const shouldBeDarkTheme = JSON.parse(persistedThemeState)
+            
+            if (shouldBeDarkTheme) {
+                console.log(this.darkThem())
+                switchToggle.checked = true;
+            } 
+        }
+    }
+    
+
+    darkThem() {
+        this.bodyRef.classList.add(Theme.DARK)
+        this.bodyRef.classList.remove(Theme.LIGHT)
+
+        localStorage.setItem('Theme-DARK','true')
+    }
+    lightThem() {
+        this.bodyRef.classList.add(Theme.LIGHT)
+        this.bodyRef.classList.remove(Theme.DARK)
+
+        localStorage.setItem('Theme-DARK','false')
+    }
+
+}
+
+const bodyColor = new BodyTheme({
+    selector: 'body',
+    className:Theme.LIGHT
+})
+
+switchToggle.addEventListener('change', onChangeTheme)
+
+function onChangeTheme(e) {
+    
+    if (switchToggle.checked  === true) {
+        bodyColor.darkThem()
+    }
+//    if (document.body.className === Theme.LIGHT) {
+//        bodyColor.darkThem()    
+//     }
+    else {
+       bodyColor.lightThem()
+    }    
+}
+
